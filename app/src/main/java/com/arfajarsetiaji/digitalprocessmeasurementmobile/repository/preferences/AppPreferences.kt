251fdaf86ac01ca7acfae9a7e0f91ef971cf6519
arfajarsetiaji <arfajarsetiaji.id@gmail.com>
2020-01-02 15:40:30 +0700

    Minor fixes

--- app/src/main/java/com/arfajarsetiaji/digitalprocessmeasurementmobile/repository/preferences/AppPreferences.kt
+++ app/src/main/java/com/arfajarsetiaji/digitalprocessmeasurementmobile/repository/preferences/AppPreferences.kt
@@ -6,12 +6,12 @@ import android.content.SharedPreferences
 object AppPreferences {
     private const val NAME = "Digital_Pocess_Measurement_Mobile"
     private const val MODE = Context.MODE_PRIVATE
-    private lateinit var preferences: SharedPreferences
+    private lateinit var sharedPreferences: SharedPreferences
     private var IS_NOT_FIRST_RUN_PREF = Pair("is_first_run", false)
     private var IS_USER_LOGGED_IN_PREF = Pair("is_user_logged_in", false)
     private var USER_WORK_CENTER_PREF = Pair("user_work_center", "general")
 
-    fun init(context: Context) { preferences = context.getSharedPreferences(NAME, MODE)}
+    fun init(context: Context) { sharedPreferences = context.getSharedPreferences(NAME, MODE)}
 
     private inline fun SharedPreferences.edit(operation: (SharedPreferences.Editor) -> Unit) {
         val editor = edit()
@@ -20,17 +20,17 @@ object AppPreferences {
     }
 
     var notFirstRun: Boolean
-        get() = preferences.getBoolean(IS_NOT_FIRST_RUN_PREF.first, IS_NOT_FIRST_RUN_PREF.second)
-        set(value) = preferences.edit { it.putBoolean(IS_NOT_FIRST_RUN_PREF.first, value)
+        get() = sharedPreferences.getBoolean(IS_NOT_FIRST_RUN_PREF.first, IS_NOT_FIRST_RUN_PREF.second)
+        set(value) = sharedPreferences.edit { it.putBoolean(IS_NOT_FIRST_RUN_PREF.first, value)
         }
 
     var userLoggedIn: Boolean
-        get() = preferences.getBoolean(IS_USER_LOGGED_IN_PREF.first, IS_USER_LOGGED_IN_PREF.second)
-        set(value) = preferences.edit { it.putBoolean(IS_USER_LOGGED_IN_PREF.first, value)
+        get() = sharedPreferences.getBoolean(IS_USER_LOGGED_IN_PREF.first, IS_USER_LOGGED_IN_PREF.second)
+        set(value) = sharedPreferences.edit { it.putBoolean(IS_USER_LOGGED_IN_PREF.first, value)
         }
 
     var userWorkCenter: String?
-        get() = preferences.getString(USER_WORK_CENTER_PREF.first, USER_WORK_CENTER_PREF.second)
-        set(value) = preferences.edit { it.putString(USER_WORK_CENTER_PREF.first, value)
+        get() = sharedPreferences.getString(USER_WORK_CENTER_PREF.first, USER_WORK_CENTER_PREF.second)
+        set(value) = sharedPreferences.edit { it.putString(USER_WORK_CENTER_PREF.first, value)
         }
 }
